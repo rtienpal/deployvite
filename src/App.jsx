2916b3cea9438a46b522d4f45d5ca59db1731ddd
rtienpal <rafaeluen@gmail.com>
2022-07-01 21:27:19 -0300

    test third deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           </button>
         </p>
         <p>
-          Edit <code>App.jsx</code> and save to test HMR 123updates.
+          Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
