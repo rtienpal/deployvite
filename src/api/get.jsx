46e4b3f0667850d889d823b1a397828eb93130b0
rtienpal <rafaeluen@gmail.com>
2022-07-03 02:59:23 -0300

    made page for first screen

--- src/api/get.jsx
+++ src/api/get.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "https://opentdb.com/api.php?amount=5",
+  baseURL: "https://api.hgbrasil.com/weather?format=json-cors&key=e5edefb6&city_name=",
 })
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,77 @@ import api from "../api/get"
 import React from "react"
 
 export default function Main() {
-  const [cityData, setCityData] = React.useState([])
+  const [cityData, setCityData] = React.useState([
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "Rio de Janeiro",
+    },
+    {
+      apiCityId: "saopaulo,SP",
+      min: "-",
+      max: "-",
+      cityName: "Sao Paulo",
+    },
+    {
+      apiCityId: "Belo%20Horizonte,MG",
+      min: "-",
+      max: "-",
+      cityName: "Belo Horizonte",
+    },
+    {
+      apiCityId: "brasilia,DF",
+      min: "-",
+      max: "-",
+      cityName: "Brasilia",
+    },
+    {
+      apiCityId: "belem,pa",
+      min: "-",
+      max: "-",
+      cityName: "Belém",
+    },
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "Salvador",
+    },
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "Curitiba",
+    },
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "Fortaleza",
+    },
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "Manaus",
+    },
+    {
+      apiCityId: "riodejaneiro,RJ",
+      min: "-",
+      max: "-",
+      cityName: "João Pessoa",
+    },
+  ])
+  console.log(cityData)
+  const [cityStatus, setCityStatus] = React.useState("idle")
 
   React.useEffect(() => {
     const fetchApi = async () => {
       try {
-        const response = await api.get("")
-        console.log(response.data)
-        setCityData(response)
+        const response = await api.get("Salvador,BA")
+        setCityData(response.data.results.city_name)
+        setCityStatus("resolved")
       } catch (err) {
         if (err.response) {
           console.log(err.response.data)
@@ -26,15 +89,62 @@ export default function Main() {
   }, [])
   return (
     <main>
-      <div className="text">Previsão do Tempo</div>
-      <form>
-        <input
-          type="text"
-          placeholder="Insira aqui o nome da cidade"
-          onChange=""
-          name="cidade"
-        ></input>
-      </form>
+      {cityStatus === "resolved" && <div>{JSON.stringify(cityData)}</div>}
+      <div className="title-city-input">
+        <div className="title">
+          <h1>Previsão do Tempo</h1>
+        </div>
+        <form>
+          <input
+            className="input"
+            type="text"
+            placeholder="Insira aqui o nome da cidade"
+            onChange=""
+            name="cidade"
+          ></input>
+        </form>
+      </div>
+      <div className="capitals capitals-grid">
+        <div className="capitals capitals-text">
+          <h2>Capitais</h2>
+        </div>
+        <div className="capitals capitals-gridtitle gridtitle-min">Min</div>
+        <div className="capitals capitals-gridtitle gridtitle-max">Max</div>
+        <div className="capitals capitals-gridtitle gridtitle-city">Cidade</div>
+        <div className="capitals capitals-gridtitle gridtitle-min">Min</div>
+        <div className="capitals capitals-gridtitle gridtitle-max">Max</div>
+        <div className="capitals capitals-gridtitle gridtitle-city">Cidade</div>
+        <div className="capitals capitals-city city-min1">8º</div>
+        <div className="capitals capitals-city city-max1">8º</div>
+        <div className="capitals capitals-city city-name1">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min2">19º</div>
+        <div className="capitals capitals-city city-max2">19º</div>
+        <div className="capitals capitals-city city-name2">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min1">8º</div>
+        <div className="capitals capitals-city city-max1">8º</div>
+        <div className="capitals capitals-city city-name1">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min2">19º</div>
+        <div className="capitals capitals-city city-max2">19º</div>
+        <div className="capitals capitals-city city-name2">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min1">8º</div>
+        <div className="capitals capitals-city city-max1">8º</div>
+        <div className="capitals capitals-city city-name1">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min2">19º</div>
+        <div className="capitals capitals-city city-max2">19º</div>
+        <div className="capitals capitals-city city-name2">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min1">8º</div>
+        <div className="capitals capitals-city city-max1">8º</div>
+        <div className="capitals capitals-city city-name1">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min2">19º</div>
+        <div className="capitals capitals-city city-max2">19º</div>
+        <div className="capitals capitals-city city-name2">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min1">8º</div>
+        <div className="capitals capitals-city city-max1">8º</div>
+        <div className="capitals capitals-city city-name1">Rio de Janeiro</div>
+        <div className="capitals capitals-city city-min2">19º</div>
+        <div className="capitals capitals-city city-max2">19º</div>
+        <div className="capitals capitals-city city-name2">Rio de Janeiro</div>
+      </div>
     </main>
   )
   // return <>{cityData && <main>{JSON.stringify(cityData)}</main>}</>
