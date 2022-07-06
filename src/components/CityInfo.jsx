95f1366cad9ae4d1cd30862e758a89cd947e8f3b
rtienpal <rafaeluen@gmail.com>
2022-07-06 15:02:17 -0300

    adding info on city container

--- src/components/CityInfo.jsx
+++ src/components/CityInfo.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function CityInfo({searchWord}) {
-    return <div className="cityInfoContainer">{searchWord}</div>
+    return <div className="cityInfoContainer">
+        <div className="cityTitle">Niterói, RJ - Brasil</div>
+        <div className="cityCurrentTemp">20ºC Nublado</div>
+        <div className="cityOtherInfoContainer">
+            <div className="minMaxContainer">
+                <div className="minMaxContainer min">16º</div>
+                <div className="minMaxContainer min">25º</div>
+            </div>
+            <div className="windHumidity">
+                <div className="windHumidity wind">Vento: 18km/h</div>
+                <div className="windHumidity humidity">89%</div>
+            </div>
+
+        </div>
+    </div>
 }
