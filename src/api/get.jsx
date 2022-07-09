e19a57d1b67b93c753bebbd6f9ea5371f566a02f
rtienpal <93673309+rtienpal@users.noreply.github.com>
2022-07-09 04:50:35 -0300

    adding key

--- src/api/get.jsx
+++ src/api/get.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-  // baseURL: "https://api.hgbrasil.com/weather?format=json-cors&key=a06531f1&city_name=",
+  baseURL:
+    "https://api.hgbrasil.com/weather?format=json-cors&key=2abe506d&city_name=",
 })
-
